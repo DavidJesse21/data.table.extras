3764b853db5e006dda87f24b913aa198442e2562
DavidJesse21 <david.jesse@gmx.net>
2021-12-30 18:36:18 +0100

    Remove `library` call in test file

--- inst/tinytest/test_setj_at.R
+++ inst/tinytest/test_setj_at.R
@@ -1,11 +1,10 @@
-library(data.table)
-
-dt_check = data.table(
+dt_check = data.table::data.table(
   a = 1:5,
   b = 1:5,
   c = 1:5,
   d = 1:5
 )
+
 
 # Test basic sanity checks
 expect_error(
@@ -19,7 +18,7 @@ expect_error(
 )
 
 # Test basic functionality
-dt1 = copy(dt_check)
+dt1 = data.table::copy(dt_check)
 setj_at(dt1, 1:2, function(j) j + 1)
 expect_equal(dt1[[1]], 2:6)
 expect_equal(dt1[[2]], 2:6)
@@ -27,7 +26,7 @@ expect_equal(dt1[[3]], 1:5)
 expect_equal(dt1[[4]], 1:5)
 rm(dt1)
 
-dt2 = copy(dt_check)
+dt2 = data.table::copy(dt_check)
 setj_at(dt2, c("a", "c"), function(j) j + 1)
 expect_equal(dt2[["a"]], 2:6)
 expect_equal(dt2[["c"]], 2:6)
@@ -37,8 +36,8 @@ rm(dt2)
 
 # Scenario: Some invalid column names or indices have been provided
 expect_message(
-  setj_at(copy(dt_check), c(1L, 5L), function(j) j + 1)
+  setj_at(data.table::copy(dt_check), c(1L, 5L), function(j) j + 1)
 )
 expect_message(
-  setj_at(copy(dt_check), c("a", "e"), function(j) j + 1)
+  setj_at(data.table::copy(dt_check), c("a", "e"), function(j) j + 1)
 )
